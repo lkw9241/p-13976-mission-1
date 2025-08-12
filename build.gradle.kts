a2a7f0b9a79afd88c60ae07536b866a7b65dade3
LeeKW <lkw9241@gmail.com>
2025-08-12 15:56:10 +0900

    Add code from p-13997-1_a

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.2.0"
+}
+
+group = "com.ll"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    testImplementation("org.assertj:assertj-core:3.27.3")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(24)
+}
---
+++ src/main/kotlin/com/ll/App.kt
@@ -0,0 +1,39 @@
+package com.ll
+
+import com.ll.domain.system.system.controller.SystemController
+import com.ll.domain.wiseSaying.wiseSaying.controller.WiseSayingController
+
+class App {
+    fun run() {
+
+        val wiseSayingController = WiseSayingController()
+        val systemController = SystemController()
+
+        println("== 명언 앱 ==")
+
+        while (true) {
+            print("명령) ")
+            val input = readlnOrNull()!!.trim() // !! 의미 nullable 빼라
+
+            val rq = Rq(input)
+
+
+
+            when (rq.action) {
+                "종료" -> {
+                    systemController.actionExit(rq)
+                    break
+                }
+                "등록" -> {
+                    wiseSayingController.actionWrite(rq)
+                }
+                "목록" -> {
+                    wiseSayingController.actionList(rq)
+                }
+                "삭제" -> {
+                    wiseSayingController.actionDelete(rq)
+                }
+            }
+        }
+    }
+}
